ac400aa589ad3a1e0516d353c2fa14e793b88d6b
unknown <travihay@gmail.com>
2024-10-07 17:10:18 -0500

    added code

--- paint-inventory-client/client/vite.config.js
+++ paint-inventory-client/client/vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:5000', // Proxy to the backend
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  build: {
+    outDir: 'dist',
+  },
 });
